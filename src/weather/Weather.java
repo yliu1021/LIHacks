a5cd7130b55cd0ad3c3f238f8e86ae27723dae44
albertzhang <aalbertzhang@gmail.com>
2016-08-28 05:03:15 -0400

    adfa

--- src/weather/Weather.java
+++ src/weather/Weather.java
@@ -13,6 +13,11 @@ public class Weather {
     private ArrayList<Integer> temperatureList = new ArrayList<Integer>();
     private ArrayList<Date> dateList = new ArrayList<Date>();
     private ArrayList<String> conditionList = new ArrayList<String>();
+    private ArrayList<Integer> humidityList = new ArrayList<Integer>();
+    private ArrayList<Integer> uviList = new ArrayList<Integer>();
+    private ArrayList<Integer> feelsLikeList = new ArrayList<Integer>();
+    private ArrayList<Integer> windSpeedList = new ArrayList<Integer>();
+    private ArrayList<Integer> windDirList = new ArrayList<Integer>();
 
     public void parseWeather() {
         JSONObject jsonObject = WeatherRequest.getWeatherJSON();
@@ -29,10 +34,23 @@ public class Weather {
             JSONObject forecast = forecasts.getJSONObject(i);
             JSONObject time = forecast.getJSONObject("FCTTIME");
             JSONObject temp = forecast.getJSONObject("temp");
+            JSONObject humid = forecast.getJSONObject("humidity");
+            JSONObject ws = forecast.getJSONObject("wspd");
+            JSONObject wd = forecast.getJSONObject("wdir");
+            JSONObject uv = forecast.getJSONObject("uvi");
+            JSONObject feel = forecast.getJSONObject("feelslike");
 
             Integer hour = time.getInt("hour");
             Integer month = time.getInt("mon");
             Integer day = time.getInt("mday");
+
+            Integer humidity = humid.getInt("humidity");
+            Integer windSpeed = ws.getInt("english");
+            String windDirection = wd.getString("dir");
+            Integer uvi = uv.getInt("uvi");
+            Integer feelslike = feel.getInt("english");
+
+
             Date date = new Date(month,day,hour);
             Integer temperature = temp.getInt("english");
             String condition = forecast.getString("condition");
@@ -44,6 +62,38 @@ public class Weather {
         this.temperatureList = temperatureList;
         this.dateList = dateList;
         this.conditionList = conditionList;
+    }
+
+    public Integer getHumidity(int month, int day, int hour) {
+        int index = getDateIndex(month, day, hour);
+        if(index == -1) return null;
+
+        return humidityList.get(index);
+    }
+    public Integer getWindSpeed(int month, int day, int hour) {
+        int index = getDateIndex(month, day, hour);
+        if(index == -1) return null;
+
+        return windSpeedList.get(index);
+    }
+    public Integer getWindDir(int month, int day, int hour) {
+        int index = getDateIndex(month, day, hour);
+        if(index == -1) return null;
+
+        return windDirList.get(index);
+    }
+    public Integer getuvi(int month, int day, int hour) {
+        int index = getDateIndex(month, day, hour);
+        if(index == -1) return null;
+
+        return uviList.get(index);
+    }
+
+    public Integer getFeelsLike(int month, int day, int hour) {
+        int index = getDateIndex(month, day, hour);
+        if(index == -1) return null;
+
+        return feelsLikeList.get(index);
     }
 
     public Integer getTemperatureOnDay(int month, int day, int hour) {
